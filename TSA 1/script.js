b47345e5a8bbc601e5a56e00fc0726f873f5fdc3
bernabhie <126488840+bernabhie@users.noreply.github.com>
2024-02-01 21:53:19 +0800

    script.js

--- TSA 1/script.js
+++ TSA 1/script.js
@@ -1,8 +1,22 @@
 // Get the button element by its id
 var button = document.getElementById("switch-button");
 
-// Define a variable to store the current color mode
-var mode = "light";
+// Function to generate a random color
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+// Function to update frame color
+function updateFrameColor() {
+  var img = document.querySelector("img");
+  var randomFrameColor = getRandomColor();
+  img.style.borderColor = randomFrameColor;
+}
 
 // Add a click event listener to the button
 button.addEventListener("click", function() {
@@ -12,58 +26,22 @@ button.addEventListener("click", function() {
   // Get all the elements that contain text
   var textElements = document.querySelectorAll("h1,h2, h3, ul, li, p, span");
 
-  // Check the current color mode
-  if (mode === "light") {
-    // Change the mode to dark
-    mode = "dark";
+  // Generate random background and font colors
+  var randomBackgroundColor = getRandomColor();
+  var randomFontColor = getRandomColor();
 
-    // Change the button text color to white
-    button.style.color = "white";
+  // Apply random colors to the body and text elements
+  body.style.backgroundColor = randomBackgroundColor;
 
-    // Change the button border color to white
-    button.style.borderColor = "white";
-
-    // Change the button background color to black
-    button.style.backgroundColor = "black";
-
-    // Remove the button background image
-    button.style.backgroundImage = "none";
-
-    // Remove the button filter
-    button.style.filter = "none";
-
-    // Change the body background color to black
-    body.style.backgroundColor = "black";
-
-    // Change the text color to white for each element
-    for (var i = 0; i < textElements.length; i++) {
-      textElements[i].style.color = "white";
-    }
-  } else {
-    // Change the mode to light
-    mode = "light";
-
-    // Change the button text color to black
-    button.style.color = "black";
-
-    // Change the button border color to black
-    button.style.borderColor = "black";
-
-    // Change the button background color to white
-    button.style.backgroundColor = "white";
-
-    // Remove the button background image
-    button.style.backgroundImage = "none";
-
-    // Remove the button filter
-    button.style.filter = "none";
-
-    // Change the body background color to white
-    body.style.backgroundColor = "white";
-
-    // Change the text color to black for each element
-    for (var i = 0; i < textElements.length; i++) {
-      textElements[i].style.color = "black";
-    }
+  for (var i = 0; i < textElements.length; i++) {
+    textElements[i].style.color = randomFontColor;
   }
+
+  // Apply random colors to the button
+  button.style.color = randomFontColor;
+  button.style.borderColor = randomFontColor;
+  button.style.backgroundColor = randomBackgroundColor;
+
+  // Update frame color
+  updateFrameColor();
 });
